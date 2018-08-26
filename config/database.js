9d7e94e1538cabe14dadaa9bb6772862d33c5154
m1rke <33702154+m1rke@users.noreply.github.com>
2018-08-26 03:38:03 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-  uri: 'mongodb://hakerin:Miremire2404@ds159856.mlab.com:59856/hakerindb',
+  uri: 'mongodb://
   secret: crypto,
   db: 'hakerindb'
 }
